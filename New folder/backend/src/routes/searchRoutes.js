dcec9d69fa8fb718ddbcf667e8828dfa36405083
Mano sai tej <yenugulamanosai@gmail.com>
2026-04-03 13:20:48 +0530

    Delete New folder directory

--- New folder/backend/src/routes/searchRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAggregatedSearch } = require('../controllers/searchController');
-const { protect } = require('../middlewares/authMiddleware');
-
-router.get('/', protect, getAggregatedSearch);
-
-module.exports = router;
